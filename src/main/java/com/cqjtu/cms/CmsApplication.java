af3337500976c2f86ea798ae7afc184a5e6dd144
pc-suwen <577014284@qq.com>
2020-10-25 10:43:10 +0800

    init cms project

---
+++ src/main/java/com/cqjtu/cms/CmsApplication.java
@@ -0,0 +1,19 @@
+package com.cqjtu.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 启动入口
+ *
+ * @author suwen
+ * @date 2020/10/25 10:41
+ */
+@SpringBootApplication
+public class CmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class, args);
+    }
+
+}
